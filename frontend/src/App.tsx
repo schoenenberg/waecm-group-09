197297706938b32c3b33813c6990eeaf3c9a72a3
s1gr1d <sigrid.huemer@gmx.net>
2020-03-24 22:48:45 +0100

    Code Formatting with Prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,65 @@
-import React, {useState} from 'react';
-import './App.css';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { uuid } from 'uuidv4';
-import { useEffect } from 'react';
-import Image from './background_image.jpg';
-import {Alert, AlertTitle} from "@material-ui/lab";
-import ApolloClient from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import {ApolloProvider, useQuery} from '@apollo/react-hooks';
-import {gql} from "apollo-boost";
+import React, { useState } from "react";
+import "./App.css";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { uuid } from "uuidv4";
+import { useEffect } from "react";
+import Image from "./background_image.jpg";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 const useStyles = makeStyles(theme => ({
   login: {
     marginTop: theme.spacing(25),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   profile_details: {
     marginTop: theme.spacing(10),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   access_denied: {
     marginTop: theme.spacing(15),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   button: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 2)
   },
   large: {
     width: theme.spacing(40),
-    height: theme.spacing(40),
+    height: theme.spacing(40)
   },
-  container:{
-    width: '100vw',
-    height: '100vh',
+  container: {
+    width: "100vw",
+    height: "100vh",
     backgroundImage: `url(${Image})`,
-    backgroundSize: 'cover',
-    headerMode: 'screen'
+    backgroundSize: "cover",
+    headerMode: "screen"
   },
-  fonts:{
+  fonts: {
     margin: 0,
-    color: 'white',
+    color: "white"
   },
-  username:{
+  username: {
     margin: 0,
-    color: 'white',
-    textAlign: 'center'
+    color: "white",
+    textAlign: "center"
   }
 }));
 
@@ -69,26 +69,28 @@ function App() {
   const [oldToken, setOldToken] = useState("");
   const [onProfileDetailPage, setOnProfileDetailPage] = useState(false);
   const [accessDenied, setAccessDenied] = useState(false);
-  const [client, setClient] = useState(new ApolloClient({
-    link: new HttpLink({
-      uri: 'http://localhost:8080/graphql',
-      headers: {
-        Authorization: `Bearer ` + token_id
-      }
-    }),
-    cache: new InMemoryCache(),
-  }));
+  const [client, setClient] = useState(
+    new ApolloClient({
+      link: new HttpLink({
+        uri: "http://localhost:8080/graphql",
+        headers: {
+          Authorization: `Bearer ` + token_id
+        }
+      }),
+      cache: new InMemoryCache()
+    })
+  );
 
   const GET_USERINFO = gql`
-   {
-    currentUser {
-      username
-      picture
+    {
+      currentUser {
+        username
+        picture
+      }
     }
-  }
-`;
+  `;
 
-  const UserInformation= () => {
+  const UserInformation = () => {
     const { loading, error, data } = useQuery(GET_USERINFO);
     if (loading) {
       return <div>Loading...</div>;
@@ -96,7 +98,16 @@ function App() {
     if (error) {
       return <div>Error!</div>;
     }
-    return <div><Avatar alt="Remy Sharp" src={data.currentUser.picture} className = {classes.large}/><h2 className={classes.username}>{data.currentUser.username}</h2></div>;
+    return (
+      <div>
+        <Avatar
+          alt="Remy Sharp"
+          src={data.currentUser.picture}
+          className={classes.large}
+        />
+        <h2 className={classes.username}>{data.currentUser.username}</h2>
+      </div>
+    );
   };
 
   const useReactPath = () => {
@@ -117,36 +128,40 @@ function App() {
   const href = useReactPath();
 
   useEffect(() => {
-    if(href.includes("id_token")){
+    if (href.includes("id_token")) {
       setOldToken(token_id);
-      if(!oldToken.includes(href.split("token=")[1])) {
+      if (!oldToken.includes(href.split("token=")[1])) {
         setOnProfileDetailPage(true);
         setTokenId(href.split("token=")[1]);
-        setClient(new ApolloClient({
-          link: new HttpLink({
-            uri: 'http://localhost:8080/graphql',
-            headers: {
-              Authorization: `Bearer ` + token_id
-            }
-          }),
-          cache: new InMemoryCache(),
-        }));
+        setClient(
+          new ApolloClient({
+            link: new HttpLink({
+              uri: "http://localhost:8080/graphql",
+              headers: {
+                Authorization: `Bearer ` + token_id
+              }
+            }),
+            cache: new InMemoryCache()
+          })
+        );
       }
     }
-    if(href.includes("access_denied")){
+    if (href.includes("access_denied")) {
       setAccessDenied(true);
     }
   }, [href, token_id, onProfileDetailPage, accessDenied, client, oldToken]);
 
-
-  function auth_url () : string {
-    return "https://waecm-sso.inso.tuwien.ac.at/auth/realms/waecm/protocol/openid-connect/auth" +
-        "?client_id=waecm"
-        +"&response_type=id_token"
-        +"&prompt=consent"
-        +"&nonce="+uuid()
-        +"&scope=openid%20profile"
-        +"&redirect_uri=http://localhost:3000";
+  function auth_url(): string {
+    return (
+      "https://waecm-sso.inso.tuwien.ac.at/auth/realms/waecm/protocol/openid-connect/auth" +
+      "?client_id=waecm" +
+      "&response_type=id_token" +
+      "&prompt=consent" +
+      "&nonce=" +
+      uuid() +
+      "&scope=openid%20profile" +
+      "&redirect_uri=http://localhost:3000"
+    );
   }
 
   function login() {
@@ -154,7 +169,9 @@ function App() {
   }
 
   function logout() {
-    window.location.replace("https://waecm-sso.inso.tuwien.ac.at/auth/realms/waecm/protocol/openid-connect/logout?post_logout_redirect_uri=http://localhost:3000");
+    window.location.replace(
+      "https://waecm-sso.inso.tuwien.ac.at/auth/realms/waecm/protocol/openid-connect/logout?post_logout_redirect_uri=http://localhost:3000"
+    );
     setOnProfileDetailPage(false);
   }
 
@@ -163,58 +180,65 @@ function App() {
   }
 
   return (
-      <ApolloProvider client={client}>
-        <Container component="main" className = {classes.container}>
-          <header>
-            <h1 className={classes.fonts}>WAECM Project: Max, Sigrid, Alicia, Elli</h1>
-          </header>
-          <Container maxWidth="xs">
-            <CssBaseline />
+    <ApolloProvider client={client}>
+      <Container component="main" className={classes.container}>
+        <header>
+          <h1 className={classes.fonts}>
+            WAECM Project: Max, Sigrid, Alicia, Elli
+          </h1>
+        </header>
+        <Container maxWidth="xs">
+          <CssBaseline />
 
-            {accessDenied &&
+          {accessDenied && (
             <div className={classes.access_denied}>
               <Alert severity="error">
                 <AlertTitle> Error </AlertTitle>
                 Unfortunately the access was denied!
               </Alert>
-              <Button fullWidth
-                      variant="contained"
-                      color="primary"
-                      className={classes.button}
-                      onClick={redirectStartPage}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={redirectStartPage}
+              >
                 Redirect to start page
               </Button>
-            </div>}
+            </div>
+          )}
 
-            {onProfileDetailPage && !accessDenied &&
-            <div className = {classes.profile_details}>
-              <UserInformation/>
+          {onProfileDetailPage && !accessDenied && (
+            <div className={classes.profile_details}>
+              <UserInformation />
               <Button
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.button}
-                  onClick={logout}>
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={logout}
+              >
                 Logout
               </Button>
             </div>
-            }
+          )}
 
-            {!onProfileDetailPage && !accessDenied &&
+          {!onProfileDetailPage && !accessDenied && (
             <div className={classes.login}>
-               <Button
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    onClick={login}
-                >
-                  Login
-                </Button>
-            </div>}
-          </Container>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={login}
+              >
+                Login
+              </Button>
+            </div>
+          )}
         </Container>
-      </ApolloProvider>
+      </Container>
+    </ApolloProvider>
   );
 }
 export default App;
