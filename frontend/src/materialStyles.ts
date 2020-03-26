472f71e09cef29a8aa5a48d9486f685fa031dba4
s1gr1d <sigrid.huemer@gmx.net>
2020-03-26 17:39:05 +0100

    Fix Centering

--- frontend/src/materialStyles.ts
+++ frontend/src/materialStyles.ts
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles(theme => ({
   container: {
+    maxWidth: "100vw",
     width: "100vw",
     height: "100vh",
     margin: 0, 
