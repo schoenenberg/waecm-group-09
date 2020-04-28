ee5dca0be79f57e3b503f2c6c0ec3648d3a4c0ba
s1gr1d <sigrid.huemer@gmx.net>
2020-04-28 16:52:22 +0200

    delete all occurences of answerCount

--- backend/src/modules/reddit-data/reddit.resolver.ts
+++ backend/src/modules/reddit-data/reddit.resolver.ts
@@ -1,15 +1,14 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { UseGuards } from "@nestjs/common";
-import { GqlAuthGuard } from "../auth/auth.guard";
-import { SubredditModel } from "./subreddit.model";
-import { RedditService } from "./reddit.service";
-import { NewSubredditInput } from "./dto/new-subreddit.input";
-import { UpdateSubredditInput } from "./dto/update-subreddit.input";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/auth.guard';
+import { SubredditModel } from './subreddit.model';
+import { RedditService } from './reddit.service';
+import { NewSubredditInput } from './dto/new-subreddit.input';
+import { UpdateSubredditInput } from './dto/update-subreddit.input';
 
 @Resolver(() => SubredditModel)
 export class RedditResolver {
-  constructor(private readonly redditService: RedditService) {
-  }
+  constructor(private readonly redditService: RedditService) {}
 
   // THIS IS JUST FOR TESTING ---- delete later --------------------------------
   @Query(() => SubredditModel)
@@ -32,17 +31,12 @@ export class RedditResolver {
 
   @Query(() => [SubredditModel])
   async allSubreddits() {
-    // TODO: answerCount is not returned, as it is stored in an array
     return this.redditService.findAll();
   }
 
   @Mutation(() => SubredditModel)
   async getSubreddit(@Args('_id') id: string) {
-    const subreddit = await this.redditService.readOne(id);
-    return {
-      ...subreddit,
-      answerCount: subreddit.answeredCommentIDs.length,
-    };
+    return await this.redditService.readOne(id);
   }
 
   @Mutation(() => SubredditModel)
@@ -50,23 +44,16 @@ export class RedditResolver {
     @Args('_id') id: string,
     @Args('input') subredditInput: UpdateSubredditInput,
   ) {
-    return await this.redditService.update(
-      id,
-      subredditInput,
-    );
+    return await this.redditService.update(id, subredditInput);
   }
 
   @Mutation(() => SubredditModel)
-  async createNewSubreddit(
-    @Args('input') subredditInput: NewSubredditInput,
-  ) {
+  async createNewSubreddit(@Args('input') subredditInput: NewSubredditInput) {
     return await this.redditService.createSubreddit(subredditInput);
   }
 
   @Mutation(() => SubredditModel)
-  async deleteSubreddit(
-    @Args('_id') id: string,
-  ) {
+  async deleteSubreddit(@Args('_id') id: string) {
     return await this.redditService.delete(id);
   }
 }
