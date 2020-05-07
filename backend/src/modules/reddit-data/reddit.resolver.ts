8a2369351acd0eb4f499dc640f3e140d5fb6b32d
Sigrid <sigrid.huemerqgmx.net>
2020-05-07 12:21:12 +0200

    delete unused query

--- backend/src/modules/reddit-data/reddit.resolver.ts
+++ backend/src/modules/reddit-data/reddit.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
-import { GqlAuthGuard } from '../auth/auth.guard';
+// import { UseGuards } from '@nestjs/common';
+// import { GqlAuthGuard } from '../auth/auth.guard';
 import { SubredditModel } from './subreddit.model';
 import { RedditService } from './reddit.service';
 import { NewSubredditInput } from './dto/new-subreddit.input';
@@ -9,25 +9,6 @@ import { UpdateSubredditInput } from './dto/update-subreddit.input';
 @Resolver(() => SubredditModel)
 export class RedditResolver {
   constructor(private readonly redditService: RedditService) {}
-
-  // THIS IS JUST FOR TESTING ---- delete later --------------------------------
-  @Query(() => SubredditModel)
-  @UseGuards(GqlAuthGuard)
-  subreddit(): SubredditModel {
-    return {
-      _id: 'gfdsdfgsdfg',
-      name: '/r/reactjs-testData',
-      description: 'sample description',
-      icon: 'https://api.adorable.io/avatars/285/abott@adorable.png',
-      answeredCommentIDs: ['1', '2', '3'],
-      active: false,
-      answer: 'test Answer',
-      keywords: ['first', 'hello', 'last'],
-      createdOn: new Date(Date.now()),
-    };
-  }
-
-  // ---------------------------------------------------------------------------
 
   @Query(() => [SubredditModel])
   async allSubreddits() {
